ec489a3435fe4b437d42cfd8537db7ebfd2fc3aa
RitaSahakyan <rita.2002.sahakyan@gmail.com>
2022-02-10 17:06:11 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Route } from "react-router-dom";
-import { routes } from "./routes";
+import {Route} from "react-router";
+import {routes} from "./routes";
 
 export const App = () => {
-  return (
-    <div>
-      {routes.map((item, index) => (
-        <Route
-          key={index}
-          path={item.path}
-          exact={item.exact}
-          children={<item.page />}
-        />
-      ))}
-    </div>
+  return(
+      <div>
+        {routes.map((item, index) => (
+            <Route
+              key={index}
+              path={item.path}
+              exact={item.exact}
+              children={<item.page />}
+            />
+        ))}
+      </div>
   );
 };
