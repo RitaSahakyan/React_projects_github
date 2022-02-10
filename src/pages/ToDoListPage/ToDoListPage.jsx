fa12c7b4d1faf860b4ad62d48d632d63c1dcc32c
RitaSahakyan <rita.2002.sahakyan@gmail.com>
2022-02-10 17:43:08 +0400

    new1

--- src/pages/ToDoListPage/ToDoListPage.jsx
+++ src/pages/ToDoListPage/ToDoListPage.jsx
@@ -1,59 +1,58 @@
-import { List } from "./components/List";
-import { ToDoListHeader } from "./components/ToDoListHeader";
-import { useState } from "react";
-import { ToDoModal } from "../../components/ToDOModal";
+import {useState} from "react";
+import {ToDoModal} from "../../components/ToDoModal/ToDoModal";
+import {ToDoListHeader} from "../components/ToDoListHeader";
+import {List} from "../components/List";
 
 const initialState = {
     list: [],
     isOpen: false,
     isEdit: false,
     isDel: false,
-};
+}
 
-export const ToDoListPage = () => {
-    const [state, setState] = useState(initialState);
-    const { list, isOpen, isEdit, isDel } = state;
-
+export const ToDoListPage  = () =>{
+    const [state, setState] = useState(initialState)
+    const {list, isOpen, isEdit, isDel}= state
     const addFunc = (value) => {
-        const listClone = JSON.parse(JSON.stringify(list));
-        listClone.push(value);
-        setState((prevState) => ({ ...prevState, list: listClone }));
-    };
+        const listClone =JSON.parse(JSON.stringify(list))
+        listClone.push(valuse)
+        setState((prevState) => ({...prevState, list: listClone}))
+    }
 
     const handleModal = (value) => {
         if (value === "open") {
-            setState((prevState) => ({ ...prevState, isOpen: true }));
-        } else if (value === "close") {
-            setState((prevState) => ({ ...prevState, isOpen: false }));
+            setState((prevState) => ({ ...prevState, isOpen: true }))
+        } else if  (value === "close") {
+            setState((prevState) => ({ ...prevState, isOpen: false }))
         }
-    };
+    }
 
-    const del = () => {
-        const listClone = JSON.parse(JSON.stringify(list));
-        listClone.splice(value, 1);
-        setState((prevState) => ({ ...prevState, list: listClone }));
-    };
+    const del = () =>{
+        const listClone = JSON.parse(JSON.stringify(list))
+        listClone.splice(value, 1)
+        setState((prevState) => ({ ...prevState, list: listClone }))
+    }
 
-    const openDelModal = (value) => {
-        setState((prevState) => ({ ...prevState, isDel: true, isEdit: false }));
-        handleModal("open");
-    };
+    const openModal = (value ) => {
+        setState((prevState) => ({ ...prevState, isDel: true, isEdit: false }))
+        handleModal("open")
+    }
 
     const openEditModal = (value) => {
         setState((prevState) => ({ ...prevState, isDel: false, isEdit: true }));
-        handleModal("open");
-    };
+        handleModal("open")
+    }
 
     return (
-        <div style={{ width: "600px" }}>
+        <div>
             <ToDoModal
-                isOpen={isOpen}
-                handleModal={handleModal}
-                isEdit={isEdit}
-                isDel={isDel}
+            isOpen={isOpen}
+            handleModal={handleModal}
+            isEdit={isEdit}
+            isDel={isDel}
             />
             <ToDoListHeader addFunc={addFunc} />
-            <List list={list} del={openDelModal} edit={edit} />
+            <List list={list} del={openModal} edit={edit} />
         </div>
-    );
-};
+    )
+}
