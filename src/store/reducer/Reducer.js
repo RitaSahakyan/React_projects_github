091c5668bfa0f43a059d36e6e1ac6b108f8e0248
RitaSahakyan <rita.2002.sahakyan@gmail.com>
2022-02-12 19:19:31 +0400

    error not resolved

--- src/store/reducer/Reducer.js
+++ src/store/reducer/Reducer.js
@@ -2,13 +2,13 @@ import {CHANGE_NAME} from "../action";
 
 
 const initialState = {
-    name:"test name 55"
+    name:"Reducer"
 }
 
 export const Reducer = (state = initialState, action) => {
     switch (action.type){
         case CHANGE_NAME:
-            return {...state, name:"new name "}
+            return {...state, name:"new Reducer"}
         default:
             return state
     }
